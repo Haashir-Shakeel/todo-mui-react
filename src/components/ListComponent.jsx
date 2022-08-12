f5fd3ce50e69c562872af668aa2dab66b2f54797
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-12 17:04:35 +0500

    Added delete and functionality

---
+++ src/components/ListComponent.jsx
@@ -0,0 +1,18 @@
+import { Delete } from "@mui/icons-material"
+import { Button, Stack } from "@mui/material"
+import { useState } from "react"
+
+export const ListComponent = (props) =>{
+    const [line,setLine] = useState(false)
+
+    const deleteFromList = ()=>{
+        console.log('delete from list working')
+        setLine(true)
+    }
+    return (
+        <Stack alignItems='center' justifyContent='flex-start' direction='row'>
+            <Button onClick={deleteFromList} sx={{marginLeft:0}}> <Delete/> </Button>
+            <li style={{textDecoration: line ? "line-through" : "none"}} key={props.index}> {props.val}</li>
+        </Stack>
+        )
+} 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,37 +1,44 @@
-import {Stack, Box, Container, Typography, Input, IconButton} from '@mui/material'
-import {Add} from '@mui/icons-material'
+import {Stack, Box, Container, Typography, Input, IconButton, Button} from '@mui/material'
+import {Add, Delete} from '@mui/icons-material'
 import { useState } from 'react'
+import { ListComponent } from './ListComponent'
 
 export const TodoList = () =>{
     const [task,setTask] = useState('')
     const [tasklist, setTasklist] = useState([])
+    
 
 
 
 
-    const handleClick = (event)=>{
+    const AddItemToList = (event)=>{
         setTasklist((prevValue)=>{
-            return [task, ...prevValue]
+            return [...prevValue, task]
         })
         console.log(tasklist)
         setTask('')
         
     }
+
+   
+
+    
+
     return(
                 
       <Container maxWidth="sm">
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
             <Typography sx={{textAlign: 'center', textDecoration:'underline'}} variant='h4' >TODO LIST</Typography>
-            <Stack justifyContent='space-evenly' direction='row'>
+            <Stack  justifyContent='space-evenly' direction='row'>
             <Input  placeholder='Enter task' value={task} onChange={e=>setTask(e.target.value)}></Input>
-            <IconButton onClick={handleClick}>
+            <IconButton onClick={AddItemToList}>
                 <Add/>
             </IconButton>
             </Stack>
             <ul>
                 {
                 tasklist.map((item,index)=>{
-                    return <li key={index}>{item}</li>
+                    return <ListComponent val={item} key={index}/>
                 })
             }
             </ul>
